7698e7781e534ba7d598942cb80b77f1ccc8fca3
Ben <ben@stolovitz.com>
2015-01-25 16:18:02 -0600

    Added mask!

--- cse452shell/brush/MyBrush.cpp
+++ cse452shell/brush/MyBrush.cpp
@@ -8,6 +8,30 @@ void MyBrush::changedBrush() {
     // this is called anytime the brush type or brush radius changes
     // it should recompute the brush mask appropriately
     const int radius = brushUI->getRadius();
+    const BrushType filter = brushUI->getBrushType();
+    
+    // Create the mask, a 1D array from 0-radius of the different values.
+    mask = std::vector<float>(radius);
+    switch (filter) {
+        case BRUSH_QUADRATIC:
+            for (float i = 0; i < radius; i += 1) {
+                mask[i] = sqrt(1 - (i * i) / ((float)radius * (float)radius));
+            }
+            break;
+            
+        case BRUSH_LINEAR:
+            for (float i = 0; i < radius; i += 1) {
+                mask[i] = ((float)radius - i) / (float)radius;
+            }
+            break;
+            
+        case BRUSH_CONSTANT:
+        default:
+            for (int i = 0; i < radius; i++) {
+                mask[i] = 1.0f;
+            }
+            break;
+    }
 }
 
 void MyBrush::drawBrush( ) {
@@ -17,6 +41,17 @@ void MyBrush::drawBrush( ) {
     const int radius = brushUI->getRadius();
     const float pixelFlow = brushUI->getFlow();
     const Color colBrush = brushUI->getColor();
+    
+    for (int i = 0; i < radius; i++) {
+        Color origL = getPixel(mouseDrag[0] - i, mouseDrag[1]);
+        Color origR = getPixel(mouseDrag[0] + i, mouseDrag[1]);
+        
+        Color interpL = colBrush * mask[i] + origL * (1.0f - mask[i]);
+        Color interpR = colBrush * mask[i] + origR * (1.0f - mask[i]);
+
+        putPixel(mouseDrag[0] - i, mouseDrag[1], interpL);
+        putPixel(mouseDrag[0] + i, mouseDrag[1], interpR);
+    }
 }
 
 void MyBrush::drawLine( ) {
