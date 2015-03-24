98132afd598160c93f49031ccd1bc0a56059e36b
Ben <ben@stolovitz.com>
2015-03-24 17:46:31 -0500

    cup?

--- cse452shell/shapes/Raw.cpp
+++ cse452shell/shapes/Raw.cpp
@@ -51,7 +51,7 @@ void Raw::draw() {
         for(size_t i = 0; i < pts.size(); i++) {
             auto pt = *pts[i];
             auto norm = *normals[i / 3];
-            glNormal3d(norm[0], norm[1], 0);
+            glNormal3d(norm[0], norm[1], norm[2]);
             glVertex3d(pt[0], pt[1], pt[2]);
         }
     glEnd();
